1078873e41d0b16f6d7ce8fd036f83d1d2eca836
Sebastien Besson <seb.besson@gmail.com>
2018-09-16 15:32:28 +0100

    Upgrade to molecule 2

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,18 +1,20 @@
-import testinfra.utils.ansible_runner
+import os
 import pytest
 
+import testinfra.utils.ansible_runner
+
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    '.molecule/ansible_inventory').get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_service_running_and_enabled(Command, Service, File):
-    if File('/etc/redhat-release').exists:
-        service = Service('haproxy')
+def test_service_running_and_enabled(host,):
+    if host.file('/etc/redhat-release').exists:
+        service = host.service('haproxy')
         assert service.is_running
         assert service.is_enabled
     else:
         # BUG: testinfra tries to use systemd
-        out = Command.check_output('service haproxy status')
+        out = host.check_output('service haproxy status')
         assert out == 'haproxy is running.'
 
 
@@ -21,36 +23,36 @@ def test_service_running_and_enabled(Command, Service, File):
     'haproxy-http.log',
     'haproxy-tcp.log',
 ])
-def test_logfiles(File, Sudo, name):
-    with Sudo():
-        assert File('/var/log/haproxy/%s' % name).exists
+def test_logfiles(host, name):
+    with host.sudo():
+        assert host.file('/var/log/haproxy/%s' % name).exists
 
 
-def test_http(Command, File, Sudo):
-    out = Command.check_output("curl -s http://localhost/")
+def test_http(host):
+    out = host.check_output("curl -s http://localhost/")
     assert '503 Service Unavailable' in out
     assert 'No server is available to handle this request.' in out
-    with Sudo():
-        log = File('/var/log/haproxy/haproxy-http.log')
+    with host.sudo():
+        log = host.file('/var/log/haproxy/haproxy-http.log')
         assert log.content_string.splitlines()[-1].endswith(
             '0/0/0/0/0 0/0 "GET / HTTP/1.1"')
 
 
-def test_https(Command, File, Sudo):
-    out = Command.check_output("curl -ks https://localhost:443/")
+def test_https(host):
+    out = host.check_output("curl -ks https://localhost:443/")
     assert '503 Service Unavailable' in out
     assert 'No server is available to handle this request.' in out
-    with Sudo():
-        log = File('/var/log/haproxy/haproxy-http.log')
+    with host.sudo():
+        log = host.file('/var/log/haproxy/haproxy-http.log')
         assert log.content_string.splitlines()[-1].endswith(
             '0/0/0/0/0 0/0 "GET / HTTP/1.1"')
 
 
-def test_tcp(Command, File, Sudo):
-    out = Command.check_output("curl -s http://localhost:81/")
+def test_tcp(host):
+    out = host.check_output("curl -s http://localhost:81/")
     assert '503 Service Unavailable' in out
     assert 'No server is available to handle this request.' in out
-    with Sudo():
-        log = File('/var/log/haproxy/haproxy-tcp.log')
+    with host.sudo():
+        log = host.file('/var/log/haproxy/haproxy-tcp.log')
         assert log.content_string.splitlines()[-1].endswith(
             '0/0/0/0/0 0/0')
